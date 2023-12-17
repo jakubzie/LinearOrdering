2749c1d5de7fd8739b655f6e95f1787242d87407
Jakub Zięba <100830752+jakubzie@users.noreply.github.com>
2023-12-17 18:51:31 +0100

    added comments inside R file

--- LinearOrderingProject/correlation_check.R
+++ LinearOrderingProject/correlation_check.R
@@ -6,6 +6,7 @@ library("readxl")
 library("dplyr")
 library("corrplot")
 
+#wczytanie danych wstępnie obrobionych w excelu
 data <- read_excel("dane_obrobione.xlsx")
 data <- data.frame(data)
 rownames(data) = data$Wojewodztwo
@@ -18,21 +19,24 @@ data1 <- select(data, -c("ludnosc","szpitale",
                         "szpitalne_oddzialy_ratunkowe","przewozy",
                         "organizacje_ratownictwo",
                         "przystanki","zgony_niemowlat"))
-#names <- colnames(data)
-#print(names)
-#colnames(data) <- c("lekarze")
+#obliczenie korelacji między zmiennymi
 cor <- cor(data)
+#przedstawienie współczynników korelacji między zmiennymi na wykresie
 corrplot(cor,method="number")
 
+#obliczenie korleacji dla wybranego zbioru zmiennych
 cor1 <- cor(data1)
+#przedstawienie korelacji na wykresie
 corrplot(cor1,method="number")
 
+#obliczenie średnich i odchyleń każdej zmiennej
 means <- c()
 std_deviations <- c()
 for(i in 1:length(data1)){
   means <- c(means, mean(data1[[i]]))
   std_deviations <- c(std_deviations, sd(data1[[i]]))
 }
+#obliczenie współczynników zmienności dla każdej zmiennej
 wspolczynnik_zmiennosci <- std_deviations/means
 wspolczynnik_zmiennosci <- data.frame(colnames(data1),wspolczynnik_zmiennosci)
 colnames(wspolczynnik_zmiennosci) <- c("zmienna","wspolczynnik_zmiennosci")
